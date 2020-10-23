8fcb964380e64ed99cd72ea654fbc1ec576c5d8c
Christian Reynolds <cmreynol1@gmail.com>
2020-10-22 21:43:30 -0400

    Added examples to print.rs

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,12 @@
 pub fn run() {
     println!("Hello from the print.rs file");
+
+    // Basic formatting
+    println!("{} is from {}", "Christian", "NC");
+
+    // Positional Arguments
+    println!("{0} is from {1} and {0} likes to {2}", "Christian", "NC", "code");
+
+    // Named Arguments
+    println!("{name} likes to {activity}", name = "Christian", activity = "code");
 }
